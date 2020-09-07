15236ba01e5e2953d50c7ec722585f7dd5a27cbf
majediver <majevski.nestor.gabriel@gmail.com>
2020-09-07 09:54:42 -0300

    home Page

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
-import 'src/pages/home_temp.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      home: HomePageTemp()
+      home: HomePage()
     );
   }
 }
