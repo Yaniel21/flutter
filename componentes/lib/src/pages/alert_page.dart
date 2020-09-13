3bd5dced97145de7a5b3ba67e4d16473662418fd
majediver <majevski.nestor.gabriel@gmail.com>
2020-09-13 08:35:31 -0300

    Animated Container-Curves

--- componentes/lib/src/pages/alert_page.dart
+++ componentes/lib/src/pages/alert_page.dart
@@ -7,12 +7,56 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_left),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+            ),
+            title: Text('Título'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                    '"Cuanto mayor es el obstáculo más gloria hay en superarlo." Molière '),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        }
     );
   }
 }
--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -6,6 +6,30 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar
+            (backgroundImage: NetworkImage('https://images-na.ssl-images-amazon.com/images/I/A1S7TdAkX4L._AC_SX425_.png'),
+            radius: 30.00,          
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20.0),
+            child: CircleAvatar(
+              child: Text(' PAL '),
+              backgroundColor: Colors.purple,
+              
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://images-na.ssl-images-amazon.com/images/I/A1S7TdAkX4L._AC_SX425_.png'),
+          placeholder:AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 100),
+        ),
       ),
     );
   }
