7d2023d0502088ae4bf9dbe52797bb96bba8569f
majediver <majevski.nestor.gabriel@gmail.com>
2020-09-13 13:11:58 -0300

    listview

---
+++ componentes/lib/src/pages/listview_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ListaPage extends StatefulWidget {
+  @override
+  _ListaPageState createState() => _ListaPageState();
+}
+
+class _ListaPageState extends State<ListaPage> {
+  List<int> _listaNumeros = [17, 22, 53, 14, 87, 88, 19];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Listas'),
+      ),
+      body: _crearLista(),
+    );
+  }
+
+  Widget _crearLista() {
+    return ListView.builder(
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext context, int index) {
+        final imagen = _listaNumeros[index];
+        return FadeInImage(
+          image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        );
+      },
+    );
+  }
+}
