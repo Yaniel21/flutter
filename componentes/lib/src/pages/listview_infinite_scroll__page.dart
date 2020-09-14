9934264f0569e847ab9c5ff211e1d311c17cf2e9
majediver <majevski.nestor.gabriel@gmail.com>
2020-09-13 23:17:46 -0300

    push refresh

--- componentes/lib/src/pages/listview_infinite_scroll__page.dart
+++ componentes/lib/src/pages/listview_infinite_scroll__page.dart
@@ -51,31 +51,17 @@ class _ListaPageState extends State<ListaPage> {
   }
 
   Widget _crearLista() {
-    return RefreshIndicator(
-      onRefresh: obtenerPagina1,
-      child: ListView.builder(
-        controller: _scrollController,
-        itemCount: _listaNumeros.length,
-        itemBuilder: (BuildContext context, int index) {
-          final imagen = _listaNumeros[index];
-
-          return FadeInImage(
-            image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
-            placeholder: AssetImage('assets/jar-loading.gif'),
-          );
-        },
-      ),
+    return ListView.builder(
+      controller: _scrollController,
+      itemCount: _listaNumeros.length,
+      itemBuilder: (BuildContext context, int index) {
+        final imagen = _listaNumeros[index];
+        return FadeInImage(
+          image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+        );
+      },
     );
-  }
-
-  Future<Null> obtenerPagina1() async {
-    final duration = new Duration(seconds: 2);
-    new Timer(duration, () {
-      _listaNumeros.clear();
-      _ultimoItem++;
-      _agregar10();
-    });
-    return Future.delayed(duration);
   }
 
   void _agregar10() {
@@ -97,8 +83,10 @@ class _ListaPageState extends State<ListaPage> {
   void respuestaHTTP() {
     _isLoading = false;
 
-    _scrollController.animateTo(_scrollController.position.pixels + 100,
-        curve: Curves.fastOutSlowIn, duration: Duration(milliseconds: 250));
+    _scrollController.animateTo(
+      _scrollController.position.pixels + 100,
+      curve: Curves.fastOutSlowIn, 
+      duration: Duration(milliseconds: 250));
     _agregar10();
   }
 
